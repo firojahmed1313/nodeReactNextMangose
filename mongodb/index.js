2270fbb9b9b52f79b9456da9e3d9ac14fae113bb
md firoj ahmed <mdfirojahmed3052000@gmail.com>
2023-11-05 05:02:20 +0000

    mongodb connect

--- mongodb/index.js
+++ mongodb/index.js
@@ -15,8 +15,39 @@ const dbConnect =()=>{
 export default dbConnect
 */
 
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
-const DB_URL:"mongodb://0.0.0.0:27017/byenow";
+const uri = "mongodb+srv://newgitpods:VL6eb0rHhdZJ1gv0@mdfirojahmed.chpjhjr.mongodb.net/testDetabase?retryWrites=true&w=majority";
 
-mongoose.connect()
+//const DB_URL:"mongodb://0.0.0.0:27017/byenow";
+
+mongoose.connect(uri).then(()=>{
+    console.info("connected");
+
+}).catch((error)=>{
+    console.log(error);
+})
+
+
+const student = new mongoose.Schema({
+    rollNo:Number,
+    name:String,
+    hight:Number
+})
+
+const Student = mongoose.model.Student || mongoose.model("Student",student);
+
+const s1=new Student({
+    id: 1,
+    name:"firoj",
+    hight:5.9
+})
+
+s1.save();
+
+const s2= Student.create({
+    
+    name:"raihan",
+    hight:5.4
+})
+
