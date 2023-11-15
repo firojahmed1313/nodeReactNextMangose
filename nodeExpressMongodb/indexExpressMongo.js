a39ea11f5a7959d01763aef1ef1a2590eb01b596
md firoj ahmed <mdfirojahmed3052000@gmail.com>
2023-11-15 06:41:07 +0000

    login logout token

--- nodeExpressMongodb/indexExpressMongo.js
+++ nodeExpressMongodb/indexExpressMongo.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import express from "express";
 import path from "path";
-
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+import bcrypt  from "bcrypt";
 const app = express();
 const port = 3000;
 
@@ -17,7 +19,8 @@ mongoose.connect(url,{
 
 const userSchema = new mongoose.Schema({
     name:String,
-    email:String
+    email:String,
+    password:String,
 })
 
 const User = mongoose.model.User || mongoose.model("User",userSchema);
@@ -31,9 +34,26 @@ const User = mongoose.model.User || mongoose.model("User",userSchema);
 
 app.use(express.urlencoded({ extended: true })); // post from data
 app.use(express.static(path.join(path.resolve(), "pubic"))); // add static file
+app.use(cookieParser())
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+
+app.get("/", async (req, res) => {
+  //res.render("index.ejs");
+  //res.redirect('/signIn');
+  const token = req.cookies.token;
+  console.log(token);
+  if(token){
+    const decode = jwt.verify(token , 'FIROJ');
+    
+    console.log(decode.id);
+     req.user1= await User.findOne({_id:decode.id});
+    //console.log(req.user);
+    res.render("logOut.ejs",{name:req.user1.name , email:req.user1.email});
+  }
+  else{
+    res.render("logIn.ejs")
+  }
+
 });
 
 
@@ -41,19 +61,66 @@ app.get("/signIn", (req, res) => {
   res.render("signIn.ejs");
 });
 app.post("/signIn", async (req, res) => {
-  res.send(
+  /*res.send(
     `<h1>   Registation Done ${req.body.name} . Your Email is ${req.body.email} </h1>`
-  );
-  console.log(req.body);
+  ); */
+  //console.log(req.body);
+  const hashPassword = await bcrypt.hash(req.body.password,10);
   const user = ({
     name:req.body.name,
-    email:req.body.email
+    email:req.body.email,
+    password:hashPassword,
   })
-  await User.create(user);
+  const user1= await User.findOne({email:user.email});
+  //console.log(user1);
+  if(user1) return res.redirect("/logIn");
+  const user2 = await User.create(user);
+  const token =  jwt.sign({ id: user2._id }, 'FIROJ');
+  res.cookie("token" , token , {
+    expires: new Date(Date.now()+1000*60*5),
+    httpOnly:true
+  })
+  res.redirect("/");
 
 });
 
 
+app.get("/logIn", (req, res) => {
+  res.render("logIn.ejs");
+});
+app.post("/logIn", async (req, res) => {
+  const email=req.body.email;
+  const password =req.body.password;
+
+  const user1= await User.findOne({email});
+
+  if(!user1) return res.redirect("/signIn");
+
+  const isPasswordMatch = await bcrypt.compare(password , user1.password);
+  
+
+  if(!isPasswordMatch) return res.redirect("/logIn" , {msg:"invalid userName or Password "});
+
+  //if(isPasswordMatch) return res.redirect("/logOut");
+
+  const token = jwt.sign({ id: user1._id }, 'FIROJ');
+  res.cookie("token" , token , {
+    expires: new Date(Date.now()+1000*60*50),
+    httpOnly:true
+  })
+  res.redirect("/");
+
+});
+
+app.get("/logOut", (req, res) => {
+  //
+  res.cookie("token" , null , {
+    expires: new Date(Date.now()),
+    httpOnly:true
+  })
+  res.redirect('/');
+  
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
